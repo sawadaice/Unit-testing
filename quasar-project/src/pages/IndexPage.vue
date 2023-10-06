64ef571d342cdf14e6fe77a48f2fd1e88221c5f3
sawadaice <icelovewoman@gmail.com>
2023-10-06 20:23:54 +0700

    ีunit testing

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: '6504101387 วีรวัฒน์ โพธิ์จวง',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
